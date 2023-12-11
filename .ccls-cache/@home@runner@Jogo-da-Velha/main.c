fcf366d977c680d68ffea0b9a55e8dd6ca6a9dd9
Bernardo-07 <84275344+Bernardo-07@users.noreply.github.com>
2023-12-11 13:57:55 +0000

    board

--- .ccls-cache/@home@runner@Jogo-da-Velha/main.c
+++ .ccls-cache/@home@runner@Jogo-da-Velha/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-
+//jogo da velha
 int main() {
-  
+  char tabuleiro[3][3];
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      tabuleiro[i][j]=' ';
+    }
+  }
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      printf("| %c",tabuleiro[i][j]);
+    }
+    printf("|\n");
+    if (i != 2){
+      printf("----------\n");
+    }
+  }
   return 0;
 }
